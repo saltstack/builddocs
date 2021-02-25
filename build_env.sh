6ece7305410d747f51c44548e3cf8acfb75f2db1
Frode Gundersen <frogunder@gmail.com>
2021-02-25 16:16:28 -0700

    Update for 3002.5

--- build_env.sh
+++ build_env.sh
@@ -5,8 +5,8 @@
 # PREVIOUS_RELEASE_DIR is the previous major release folder (2019.2), it is the link that the first tab redirects to on https://docs.saltstack.com/en/latest/contents.html (https://docs.saltstack.com/en/2019.2/)
 # See https://github.com/saltstack/salt/blob/master/doc/conf.py for more information
 
-export LATEST_RELEASE='3002.2'
-export PREVIOUS_RELEASE="${PREVIOUS_RELEASE:-3001.4}"
+export LATEST_RELEASE='3002.5'
+export PREVIOUS_RELEASE="${PREVIOUS_RELEASE:-3001.6}"
 export PREVIOUS_RELEASE_DIR="${PREVIOUS_RELEASE_DIR:-3001}"
 
 mkdir -p /usr/share/fonts/truetype
