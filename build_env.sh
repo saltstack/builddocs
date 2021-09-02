1494d1ffc6c64370c4b0166c05551e77fe627b35
ScriptAutomate <derek@icanteven.io>
2021-09-02 14:15:11 -0500

    Update buildenv for latest 3003.3 and 3002.7

--- build_env.sh
+++ build_env.sh
@@ -5,8 +5,8 @@
 # PREVIOUS_RELEASE_DIR is the previous major release folder (2019.2), it is the link that the first tab redirects to on https://docs.saltstack.com/en/latest/contents.html (https://docs.saltstack.com/en/2019.2/)
 # See https://github.com/saltstack/salt/blob/master/doc/conf.py for more information
 
-export LATEST_RELEASE='3003.2'
-export PREVIOUS_RELEASE="${PREVIOUS_RELEASE:-3002.6}"
+export LATEST_RELEASE='3003.3'
+export PREVIOUS_RELEASE="${PREVIOUS_RELEASE:-3002.7}"
 export PREVIOUS_RELEASE_DIR="${PREVIOUS_RELEASE_DIR:-3002}"
 
 mkdir -p /usr/share/fonts/truetype
