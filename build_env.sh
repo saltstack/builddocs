f14f7281519255d21bb71fceec26d49e23c94d0c
Megan Wilhite <mwilhite@vmware.com>
2022-06-17 08:48:17 -0600

    Pin jinja as temporary workaround

--- build_env.sh
+++ build_env.sh
@@ -20,3 +20,4 @@ python3 -m venv venv
 . venv/bin/activate
 pip install -U pip setuptools wheel
 (pip install -r /root/salt/requirements/static/ci/py3.8/docs.txt && pip install -r /root/salt/requirements/static/pkg/py3.8/linux.txt) || pip install -r /root/salt/requirements/static/ci/py3.8/docs.txt || pip install -r /root/salt/requirements/static/py3.8/docs.txt || (pip install distro && pip install -r /root/salt/requirements/static/py3.7/docs.txt)
+pip install jinja2==3.0.3
