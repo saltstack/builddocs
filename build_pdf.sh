0e7af83a319ff0146b2a41b65f69f2c1621231ec
ScriptAutomate <derek@icanteven.io>
2022-12-21 19:19:49 -0600

    Update to publish without IAM creds

--- build_pdf.sh
+++ build_pdf.sh
@@ -10,5 +10,5 @@ tail -n 200 /root/salt/doc/_build/latex/Salt.log
 
 echo "### Testing PDF ###"
 pdfinfo /root/salt/doc/_build/latex/Salt.pdf
-mkdir -p ./public/en/pdf/
-cp /root/salt/doc/_build/latex/Salt.pdf "./public/en/pdf/Salt-${WEBSITE_POINT_RELEASE}.pdf"
+mkdir -p ./public/pdf/
+cp /root/salt/doc/_build/latex/Salt.pdf "./public/pdf/Salt-${WEBSITE_POINT_RELEASE}.pdf"
