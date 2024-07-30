19c18bfdc063fc69cbb798862a2c429615901342
Derek <4846166-ScriptAutomate@users.noreply.gitlab.com>
2024-07-30 20:50:13 +0000

    Fix salt module pip not generating

--- build_env.sh
+++ build_env.sh
@@ -20,4 +20,5 @@ python3 -m venv venv
 . venv/bin/activate
 pip install -U pip setuptools wheel
 (pip install -r /root/salt/requirements/static/ci/py3.10/docs.txt && pip install -r /root/salt/requirements/static/pkg/py3.10/linux.txt) | (pip install -r /root/salt/requirements/static/ci/py3.10/docs.txt && pip install -r /root/salt/requirements/static/pkg/py3.10/linux.txt) || pip install -r /root/salt/requirements/static/ci/py3.10/docs.txt || pip install -r /root/salt/requirements/static/py3.10/docs.txt
-pip install jinja2==3.0.3
+# pip install jinja2==3.0.3
+pip install jaraco.text==3.14.0
--- build_epub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ex
-
-. ./build_env.sh
-make -C /root/salt/doc epub
-mkdir -p ./public/epub/
-cp /root/salt/doc/_build/epub/Salt.epub "./public/epub/Salt-${WEBSITE_POINT_RELEASE}.epub"
