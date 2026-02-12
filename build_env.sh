237bb4134e6f1ce96e3e8f25179374ae8bb4fb21
Twangboy <shane.d.lee@gmail.com>
2026-02-12 16:50:28 -0700

    Latest 3006.21 and 3007.13 releases

--- build_env.sh
+++ build_env.sh
@@ -5,8 +5,8 @@
 # PREVIOUS_RELEASE_DIR is the previous major release folder (2019.2), it is the link that the first tab redirects to on https://docs.saltstack.com/en/latest/contents.html (https://docs.saltstack.com/en/2019.2/)
 # See https://github.com/saltstack/salt/blob/master/doc/conf.py for more information
 
-export LATEST_RELEASE='3007.12'
-export PREVIOUS_RELEASE="${PREVIOUS_RELEASE:-3006.20}"
+export LATEST_RELEASE='3007.13'
+export PREVIOUS_RELEASE="${PREVIOUS_RELEASE:-3006.21}"
 export PREVIOUS_RELEASE_DIR="${PREVIOUS_RELEASE_DIR:-3006}"
 
 mkdir -p /usr/share/fonts/truetype
