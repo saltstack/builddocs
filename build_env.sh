6c96591621be5469aafd37fe2305dc95937a24ab
sb002465 <sujit.bangera@broadcom.com>
2026-04-23 16:17:22 -0700

    update salt minion release

--- build_env.sh
+++ build_env.sh
@@ -6,7 +6,7 @@
 # See https://github.com/saltstack/salt/blob/master/doc/conf.py for more information
 
 export LATEST_RELEASE='3007.13'
-export PREVIOUS_RELEASE="${PREVIOUS_RELEASE:-3006.23}"
+export PREVIOUS_RELEASE="${PREVIOUS_RELEASE:-3006.24}"
 export PREVIOUS_RELEASE_DIR="${PREVIOUS_RELEASE_DIR:-3006}"
 
 mkdir -p /usr/share/fonts/truetype
