d4e972224490bbe6264ad1b961e05e04470669b1
Twangboy <shane.d.lee@gmail.com>
2026-07-01 13:57:19 -0600

    Update 3006 LTS to 3006.27

--- build_env.sh
+++ build_env.sh
@@ -6,7 +6,7 @@
 # See https://github.com/saltstack/salt/blob/master/doc/conf.py for more information
 
 export LATEST_RELEASE='3008.1'
-export PREVIOUS_RELEASE="${PREVIOUS_RELEASE:-3006.26}"
+export PREVIOUS_RELEASE="${PREVIOUS_RELEASE:-3006.27}"
 export PREVIOUS_RELEASE_DIR="${PREVIOUS_RELEASE_DIR:-3006}"
 
 mkdir -p /usr/share/fonts/truetype
